bc5908a4ad8d4ba32094205747d50a233e995b86
rainyNighti <1219919167@qq.com>
2023-04-01 00:41:18 +0800

    Transformer

--- Transformer/Transformer.py
+++ Transformer/Transformer.py
@@ -1,7 +1,4 @@
-# code by Tae Hwan Jung(Jeff Jung) @graykode, Derek Miller @dmmiller612
-# Reference : https://github.com/jadore801120/attention-is-all-you-need-pytorch
-#           https://github.com/JayParks/transformer
-# 本代码借鉴自以下地址，加了中文注释，并修改了我认为不合理的地方（修改的地方有标注）
+# 本代码借鉴自以下地址，加了中文注释，并修改了我认为不合理的地方
 # https://github.com/graykode/nlp-tutorial/blob/master/5-1.Transformer/Transformer.ipynb
 import numpy as np
 import torch
@@ -28,14 +25,12 @@ def get_attn_pad_mask(seq_q, seq_k):
 
 
 """
-    这里的n_position是输入句子的最大长度加一（比如我们的例子中输入是“我爱你”,加一个终止符P，n_position=4）
-    每一个词首先都会被one-hot编码，这里最大长度4，所以两位就可以表示，“我爱你P”的编码对照词表为“01 10 11 00”,十进制就是“1230”，输入的数据就表示为[[1, 2, 3, 0]]，这里两个括号是batch_size=1
-    接着我们通过一个nn.embedding层，将one-hot编码（两位表示）转变为词嵌入（d_model=512位表示），也就是说输入从1*4转换为了1*4*512，一个词用512位表示
+    这里的n_position是输入句子的最大长度（比如我们的例子中输入是“我爱你P”，n_position=4）
+    每一个词首先都会被one-hot编码（这里为了省事用的是顺序编码但这是不对的），顺序编码后“我爱你P”的编码对照词表为“1230”，输入的数据就表示为[[1, 2, 3, 0]]，这里两个括号是batch_size=1
+    接着我们通过一个nn.embedding层，将顺序编码（1位表示）转变为词嵌入（d_model=512位表示），也就是说输入从1*4转换为了1*4*512，一个词用512位表示
     因此，对于第一个词的位置0，还需要遍历d_model=512，求这个词的实际编码的各个位置，也就是get_immutable_vector（）函数
     最后返回的位置编码也是一个1*4*512的矩阵，表达了每个词编码的位置
 """
-
-
 def get_sinusoid_encoding_table(n_position, d_model):
     # 这个内部函数获取的position是输入句子中一个词的position，
     def get_immutable_vector(position):
@@ -75,7 +70,7 @@ class ScaledDotProductAttention(nn.Module):
         scores.masked_fill_(attn_mask, 1e-9)
         attn = nn.Softmax(dim=-1)(scores)
         context = torch.matmul(attn, V)
-        return context, attn
+        return context
 
 
 class FeedForwardNet(nn.Module):
@@ -105,27 +100,27 @@ class MultiHeadAttn(nn.Module):
     def forward(self, Q, K, V, attn_mask):
         residual, batch_size = Q, Q.size(0)
         # Q: [batch_size x len_q x d_model], K: [batch_size x len_k x d_model], V: [batch_size x len_k x d_model]
-        # 用具体的数字说明Xw (4*513  513*1 = 4*1)：对于encoder层，自注意力：输入Q=K=V，大小1*4*512
-        # W_Q(Q)，就是一个线性映射，从512个输入映射到d_k*n_heads=512（这两个都是自定义参数）个输出，这里的线性方程表示成矩阵就是：512*1的一个参数矩阵
-        # 注意这里  不同于  线性回归，输入Q=K=V（1*4*512），线性回归，是把矩阵的每一行，当做一个样本，然后y=Xw (4*513  513*1 = 4*1)，得到每个样本经过神经元后的输出，然后求损失函数这样
+        # 对于encoder层，自注意力：输入Q=K=V，大小1*4*512
+        # W_Q(Q)，就是一个线性映射，从512个输入映射到d_k*n_heads=512（这两个都是自定义参数）个输出
+        # 注意这里  不同于  线性回归，对于输入Q=K=V（1*4*512），线性回归，是把矩阵的每一行，当做一个样本，然后y=Xw (4*513  513*1 = 4*1)，得到每个样本经过神经元后的输出，然后求损失函数这样
         # 这里的Q（1*4*512）我们，进行W_Q(Q)只是把他丢进去做一个线性变换输出的结果是（1*4*（d_k*n_heads））
         q_s = self.W_Q(Q).view(batch_size, -1, n_heads, d_k).transpose(1, 2)  # q_s: [batch*n_heads*len_q*d_k]
-        # 至于后面为什么要这样切(view函数)再转置，而不是直接像下面这样切，view()中，第一刀先把整个数据分成各个batch_size肯定没问题
-        #   然后第二刀，其实是在分割各个单词，也就是说多头的q_i, k_i, v_i来自的是一个句子的每个单词的某些部分，比如“我爱你P”，每个单词由512个数字表示，分8头，第一头就是“我”里取0-64位+“爱”里取0-64位+“你”里取0-64位+“P”里取0-64位
-        #   最后的d_k这一刀，则是为了保证QK之间相乘时不报错
-        #   而下面这个形式的分割就是：“我爱你P”，第一个头“我”里面取512个，也就是第一个头就处理“我”这个单词，并不是我们想要的，论文中想要的是多头自注意力能够从多个角度（子空间）看待问题
+        # 至于后面为什么要这样切(view函数)再转置，而不是直接像下面这样切
         # q_s = self.W_Q(Q).view(batch_size, n_heads, -1, d_k)  # q_s: [batch*n_heads*len_q*d_k]
         # print(torch.allclose(q_s, q_t, rtol=1e-5))  # False
+        #   view()中，第一刀先把整个数据分成各个batch_size肯定没问题
+        #   然后第二刀，其实是在分割各个单词，也就是说多头的q_i, k_i, v_i来自的是一个句子的每个单词的某些部分，比如“我爱你P”，每个单词由512个数字表示，分8头，第一头就是“我”里取0-64位+“爱”里取0-64位+“你”里取0-64位+“P”里取0-64位
+        #   最后的d_k这一刀，则是为了保证QK之间相乘时不报错
+        #   而第二个形式的分割就是：“我爱你P”，第一个头“我”里面取512个，也就是第一个头就只处理“我”这个单词，并不是我们想要的，论文中想要的是多头自注意力能够从多个角度（子空间）看待问题
 
         k_s = self.W_K(K).view(batch_size, -1, n_heads, d_k).transpose(1, 2)  # k_s: [batch*n_heads*len_k*d_k]
         v_s = self.W_V(V).view(batch_size, -1, n_heads, d_v).transpose(1, 2)  # v_s: [batch*n_heads*len_k*d_v]
 
-        attn_mask = attn_mask.unsqueeze(1).repeat(1, n_heads, 1,
-                                                  1)  # attn_mask : [batch_size x n_heads x len_q x len_k]
+        attn_mask = attn_mask.unsqueeze(1).repeat(1, n_heads, 1, 1)  # attn_mask : [batch_size x n_heads x len_q x len_k]
 
         # context: [batch_size x n_heads x len_q x d_v], attn: [batch_size x n_heads x len_q(=len_k) x len_k(=len_q)]
-        context, attn = ScaledDotProductAttention()(q_s, k_s, v_s, attn_mask)
-        context = context.transpose(1, 2).contiguous().view(batch_size, -1, n_heads * d_v)  # context: [batch_size x len_q x n_heads * d_v]
+        context = ScaledDotProductAttention()(q_s, k_s, v_s, attn_mask)
+        context = context.transpose(1, 2).contiguous().view(batch_size, -1, n_heads * d_v)  # context: [batch_size x len_q x n_heads x d_v]
         output = self.linear(context)
         return self.layer_norm(output + residual)  # output: [batch_size x len_q x d_model]
 
@@ -160,14 +155,14 @@ class Encoder(nn.Module):
     def __init__(self):
         super(Encoder, self).__init__()
         self.src_emb = nn.Embedding(src_vocab_size, d_model)
-        # 这一行首先nn.Embedding.from_pretrained()就是用一个预训练（给定的）词向量索引矩阵，来转换输入的文本
-        # src_vocab_size代表为每一个输入词典中的词，都生成一个位置索引
+        # 这一行首先nn.Embedding.from_pretrained()就是用一个给定的词向量索引矩阵，来转换输入的文本
+        # src_vocab_size代表为每一个输入词典中的词，每个词用d_model个数字表示，每个数字，都生成一个位置索引
         # 这一行比较复杂，下面有等价的写法（forward里），比较好理解
         self.pos_emb = nn.Embedding.from_pretrained(get_sinusoid_encoding_table(src_vocab_size, d_model), freeze=True)
         self.layers = nn.ModuleList([Encoderlayer() for _ in range(n_layers)])
 
     def forward(self, enc_inputs):
-        # 这里写的真的非常巧妙，其实就是定义了一个每个编码位置的索引矩阵：1*4*512（src_vocab*d_model），根据论文中的方法给每个词的每一个位置一个编码
+        # 这里写的真的非常巧妙，其实就是定义了一个每个编码位置的索引矩阵：1*4*512（batch_size*src_vocab*d_model），根据论文中的方法给每个词的每一个位置一个编码
         enc_outputs = self.src_emb(enc_inputs) + self.pos_emb(enc_inputs)
         # 也可以这样写，更容易理解：
         """
@@ -211,7 +206,6 @@ class Transformer(nn.Module):
     整体看Transformer分为编码，解码，以及结果输出三个模块
     于是分别定义如下
     """
-
     def __init__(self):
         super(Transformer, self).__init__()
         self.encoder = Encoder()
@@ -241,7 +235,7 @@ if __name__ == '__main__':
 
     d_model = 512  # 输入编码的词向量的长度，以及模型在运行过程中的各个层输入输出向量的维度
     d_ff = 2048  # 2层全连接层的中间维度
-    d_k = d_v = 64  # 设置的k, v矩阵的长度
+    d_k = d_v = 64  # 设置的q=k, v矩阵线性转换后的维度
     n_layers = 6  # 编码、解码层重复的次数
     n_heads = 8  # 几头注意力机制
     model = Transformer()
